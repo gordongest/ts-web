25da3f8c3f692ace9ae53bd35b967c45aa8f3303
gordongest <gordon.gest@gmail.com>
2021-03-27 17:14:42 -0600

    creating class UserForm, implemented template method

---
+++ src/views/UserForm.ts
@@ -0,0 +1,14 @@
+export class UserForm {
+  parent: Element;
+
+  template(): string {
+    return `
+      <div>
+        <h1>User Form</h1>
+        <input />
+      </div>
+    `;
+  }
+
+  render() {}
+}
