e305d13ca1eb538a7f233c66d0bda5d5857743d3
gordongest <gordon.gest@gmail.com>
2021-03-29 17:22:08 -0600

    cleanup

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,5 +1,5 @@
-import { View } from './View';
 import { User, UserProps } from '../models/User';
+import { View } from './View';
 
 export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
