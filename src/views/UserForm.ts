a4d22ed9dc3f9523abedb760a6cef9b2d5ba1b11
gordongest <gordon.gest@gmail.com>
2021-03-29 17:00:52 -0600

    refactored to use abstract View class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,34 +1,11 @@
-import { User } from '../models/User';
 import { View } from './View';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick
     };
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
-
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
   }
 
   onSetAgeClick = (): void => {
@@ -56,17 +33,5 @@ export class UserForm {
         <button class="set-age">set random age</button>
       </div>
     `;
-  }
-
-  render(): void {
-    const templateElement = document.createElement('template'); /* create an element of type template */
-
-      templateElement.innerHTML = this.template(); /* set its inner content to the output of template method */
-
-      this.bindEvents(templateElement.content); /* bind events */
-
-      this.parent.innerHTML = ''; /* empty the parent element */
-
-      this.parent.append(templateElement.content); /* append template to the parent element */
   }
 }
--- src/views/View.ts
+++ src/views/View.ts
@@ -1,3 +1,40 @@
+import { User } from '../models/User';
+
 export abstract class View {
-  
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  abstract eventsMap(): { [key: string] : () => void }
+  abstract template(): string
+
+  bindModel(): void {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template'); /* create an element of type template */
+
+      templateElement.innerHTML = this.template(); /* set its inner content to the output of template method */
+
+      this.bindEvents(templateElement.content); /* bind events */
+
+      this.parent.innerHTML = ''; /* empty the parent element */
+
+      this.parent.append(templateElement.content); /* append template to the parent element */
+  }
 }
